c2aba2ea1b9f8da2e7dfdea7d21257082cd5cdc8
MinaNagy99 <116976297+MinaNagy99@users.noreply.github.com>
2024-05-28 10:58:28 +0300

    Update ProtectedRoute.jsx

--- src/Component/ProtectedRoute/ProtectedRoute.jsx
+++ src/Component/ProtectedRoute/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 
 export default function ProtectedRoute(props) {
- if (localStorage.getItem('userToken') == null) {
+ if (localStorage.getItem('userToken') =! null) {
    return <Navigate to='/login'/>
  }
  else{
